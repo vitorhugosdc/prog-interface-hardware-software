c8c702a67d4eaf60ee3deb3122f197aaacb67436
Vitor Hugo <vitorvitinho9900@hotmail.com>
2023-10-14 22:00:46 -0300

    área do triangulo

--- calculadora.s
+++ calculadora.s
@@ -1,7 +1,7 @@
 .section .data
     prompt:          .asciz "Digite o próximo número: "
     num_prompt:      .asciz "Quantos números você deseja inserir? "
-    op_prompt:       .asciz "Digite a operação (+, -, *, /): " # Adicionei a opção de divisão aqui
+    op_prompt:       .asciz "Digite a operação (+, -, *, /, t): " # Adicionei a opção de divisão aqui
     scan_format:     .asciz "%lf"
     scan_format_int: .asciz "%d"
     scan_op_format:  .asciz " %c"
@@ -71,6 +71,8 @@ ask_operation:
     je perform_multiplication
     cmpb $'/', operation
     je perform_division
+    cmpb $'t', operation
+    je calculate_triangle_area
 
     # Se chegou aqui, a operação é inválida
     jmp invalid_op
@@ -167,6 +169,19 @@ division_by_zero:
     addl $4, %esp
     jmp end_program
     
+calculate_triangle_area:
+    leal numbers, %edi    # Apontar para o começo dos números
+    fldl (%edi)           # Carregar a base (primeiro número)
+    addl $8, %edi
+    fldl (%edi)           # Carregar a altura (segundo número)
+    fmulp                 # Multiplicar: base x altura
+    fld1                 
+    fld1
+    faddp                 # Adicionar 1.0 + 1.0 para obter 2.0
+    fdivrp                # Dividir (base x altura) por 2
+    fstpl result          # Armazenar o resultado
+    jmp print_result
+
 invalid_op:
     pushl $err_msg
     call printf
